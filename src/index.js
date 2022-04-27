568d1e73cc33af2c82a32dee51fc050e7f325d95
fenix1851 <foxyess@mail.ru>
2022-04-27 21:57:17 +0300

    added initial message

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-a
+const {Telegraf} = require('telegraf')
 
+
+const init = async (bot)=>{
+
+}
+
+init(new Telegraf("5302425236:AAHalUIVTdjs3dM-hkbEaHsojjs7RYGR4gA",{polling:true})).then(async(bot)=>{
+    await bot.launch()
+    console.log(`Launched${new Date}`)
+})
